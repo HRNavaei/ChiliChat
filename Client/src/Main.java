595d078fa33f75e5bf99a1894bb5ba7a0813095d
Hamidreza Navaei <58721554+HRNavaei@users.noreply.github.com>
2023-08-17 17:22:59 +0330

    Delete Client directory

--- Client/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String... args) throws IOException {
-        Client client = new Client("localhost",50000);
-        client.start();
-    }
-}
